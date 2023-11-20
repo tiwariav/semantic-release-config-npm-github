d7dd5898d4820ade167404d7d1441549609ea63c
Arvind Tiwari <tiwariav@gmail.com>
2023-11-20 05:48:55 +0530

    refactor: remove unused files

--- index.js
+++ index.js
@@ -45,11 +45,7 @@ module.exports = {
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
-    ...(process.env["INPUT_GITHUB-TOKEN"] || process.env["GITHUB_TOKEN"]
-      ? ["@semantic-release/github"]
-      : []),
-    ...(process.env["INPUT_NPM-TOKEN"] || process.env["NPM_TOKEN"]
-      ? ["@semantic-release/npm"]
-      : []),
+    ...(process.env.GITHUB_TOKEN ? ["@semantic-release/github"] : []),
+    ...(process.env.NPM_TOKEN ? ["@semantic-release/npm"] : []),
   ],
 };
